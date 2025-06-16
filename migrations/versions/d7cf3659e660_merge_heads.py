b9e059bc37d342499198dce4c4cdd4f8947d7c87
cabralipe <cabralipe@outlook.com>
2025-06-16 14:34:25 -0300

    migrations teste final

---
+++ migrations/versions/d7cf3659e660_merge_heads.py
@@ -0,0 +1,24 @@
+"""Merge heads
+
+Revision ID: d7cf3659e660
+Revises: 
+Create Date: 2025-06-16 14:14:24.756587
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd7cf3659e660'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
