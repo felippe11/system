b45178719ce698910f5e0d9599df69c58c05a284
cabralipe <98368563+cabralipe@users.noreply.github.com>
2025-06-22 10:27:39 -0300

    Fix syntax error in populate_script

--- populate_script.py
+++ populate_script.py
@@ -729,7 +729,7 @@ def criar_checkins(inscricoes, oficinas, eventos):
             if inscricao.oficina_id:
                 # Checkin em oficina
                 oficina = next((o for o in oficinas if o.id == inscricao.oficina_id), None)
-                if oficina && oficina.dias:
+                if oficina and oficina.dias:
                     # Pega um dia aleatório da oficina
                     dia = random.choice(oficina.dias)
                     
