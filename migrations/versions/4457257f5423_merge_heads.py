c7baebd3969b144ff0001ba6621257d67980a616
felippe11 <andrefelippe11@hotmail.com>
2025-06-27 14:57:12 -0300

    mod migration

---
+++ migrations/versions/4457257f5423_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 4457257f5423
+Revises: ffbc68259c84, 3e509fe31b3c
+Create Date: 2025-06-27 14:19:08.328461
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4457257f5423'
+down_revision = ('ffbc68259c84', '3e509fe31b3c')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
