e0eff23ff67e7fbf02c90282d0c79b102c633ee7
cabralipe <cabralipe@outlook.com>
2025-07-01 00:49:29 -0300

    correcoes de commits

---
+++ migrations/versions/0f05439c7bc2_merge_heads_revisor_process.py
@@ -0,0 +1,24 @@
+"""merge heads revisor_process
+
+Revision ID: 0f05439c7bc2
+Revises: 0cefe1a160c4, 28df0170395a
+Create Date: 2025-07-01 00:23:21.244231
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0f05439c7bc2'
+down_revision = ('0cefe1a160c4', '28df0170395a')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
