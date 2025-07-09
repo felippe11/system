e66709b2bcd88ec32122f8821704b1562cef0d50
cabralipe <cabralipe@outlook.com>
2025-07-08 21:57:09 -0300

    merge

---
+++ migrations/versions/2273dba1d6d7_unifica_múltiplos_heads.py
@@ -0,0 +1,24 @@
+"""Unifica múltiplos heads
+
+Revision ID: 2273dba1d6d7
+Revises: 4292a21d6978, a9b024c1d234
+Create Date: 2025-07-08 21:53:02.042561
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2273dba1d6d7'
+down_revision = ('4292a21d6978', 'a9b024c1d234')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
