56163b3e126400129b24fc96ec02a3ad6cfa0ccd
cabralipe <98368563+cabralipe@users.noreply.github.com>
2025-08-11 12:51:06 -0300

    Add foreign key to AuditLog submission

---
+++ migrations/versions/9bac7f9f0073_base_revision.py
@@ -0,0 +1,20 @@
+"""baseline revision
+
+Revision ID: 9bac7f9f0073
+Revises: 
+Create Date: 2024-01-01 00:00:00.000000
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+revision = "9bac7f9f0073"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade() -> None:
+    pass
+
+def downgrade() -> None:
+    pass
