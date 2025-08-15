0eb4be8617c8b3831f6056ced3e0a65ff42efdab
cabralipe <cabralipe@outlook.com>
2025-08-15 20:06:39 -0300

    alembic: merge final -> 25eb3573df18

---
+++ migrations/versions/25eb3573df18_merge_final_d7bf3ad7244c_2b05f62d38d0.py
@@ -0,0 +1,24 @@
+"""merge final: d7bf3ad7244c + 2b05f62d38d0
+
+Revision ID: 25eb3573df18
+Revises: d7bf3ad7244c, 2b05f62d38d0
+Create Date: 2025-08-15 20:05:14.534168
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '25eb3573df18'
+down_revision = ('d7bf3ad7244c', '2b05f62d38d0')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
