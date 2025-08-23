c495f0a78c34feee77db77f3976eb6411e61aee9
cabralipe <cabralipe@outlook.com>
2025-08-23 11:24:47 -0300

    TRAE2

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,6 @@ from .user import *  # noqa: F401,F403
 from .event import *  # noqa: F401,F403
 from .review import *  # noqa: F401,F403
 
+# Importação explícita do ParticipanteEvento para garantir disponibilidade
+from .event import ParticipanteEvento
+
