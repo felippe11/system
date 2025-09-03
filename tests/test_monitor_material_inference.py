a3b65fec6d285c81271c5855fa82ccf657c51e2d
cabralipe <98368563+cabralipe@users.noreply.github.com>
2025-09-03 10:31:11 -0300

    Test monitor material listing without cliente_id

--- tests/test_monitor_material_inference.py
+++ tests/test_monitor_material_inference.py
@@ -1,5 +1,6 @@
 import pytest
 import os
+
 os.environ.setdefault('SECRET_KEY', 'test')
 os.environ.setdefault('DB_PASS', 'test')
 os.environ.setdefault('GOOGLE_CLIENT_ID', 'x')
@@ -30,80 +31,54 @@ def app():
     with app.app_context():
         db.create_all()
 
-        cliente1 = Cliente(nome='Cliente', email='c@test', senha='x')
-        cliente2 = Cliente(nome='Outro', email='o@test', senha='x')
-        db.session.add_all([cliente1, cliente2])
+        cliente = Cliente(nome='Cliente', email='c@test', senha='x')
+        db.session.add(cliente)
         db.session.commit()
 
-        polo_a = Polo(nome='Polo A', cliente_id=cliente1.id, ativo=True)
-        polo_b = Polo(nome='Polo B', cliente_id=cliente1.id, ativo=True)
-        polo_c = Polo(nome='Polo C', cliente_id=cliente2.id, ativo=True)
-        db.session.add_all([polo_a, polo_b, polo_c])
+        polo = Polo(nome='Polo A', cliente_id=cliente.id, ativo=True)
+        db.session.add(polo)
         db.session.commit()
 
-        mat_a = Material(
+        material = Material(
             nome='Material A',
-            polo_id=polo_a.id,
-            cliente_id=cliente1.id,
+            polo_id=polo.id,
+            cliente_id=cliente.id,
             quantidade_inicial=1,
             quantidade_atual=1,
             quantidade_minima=0,
         )
-        mat_b = Material(
-            nome='Material B',
-            polo_id=polo_b.id,
-            cliente_id=cliente1.id,
-            quantidade_inicial=1,
-            quantidade_atual=1,
-            quantidade_minima=0,
-        )
-        mat_c = Material(
-            nome='Material C',
-            polo_id=polo_c.id,
-            cliente_id=cliente2.id,
-            quantidade_inicial=1,
-            quantidade_atual=1,
-            quantidade_minima=0,
-        )
-        db.session.add_all([mat_a, mat_b, mat_c])
+        db.session.add(material)
         db.session.commit()
 
-        monitor1 = Monitor(
-            nome_completo='Monitor Um',
+        monitor_linked = Monitor(
+            nome_completo='Monitor Ligado',
             curso='C',
             carga_horaria_disponibilidade=10,
             dias_disponibilidade='segunda',
             turnos_disponibilidade='manha',
-            email='m1@test',
+            email='ml@test',
             telefone_whatsapp='1',
-            codigo_acesso='ABC123',
-            cliente_id=cliente1.id,
+            codigo_acesso='XYZ123',
         )
-        monitor2 = Monitor(
-            nome_completo='Monitor Dois',
+        monitor_unlinked = Monitor(
+            nome_completo='Monitor Solto',
             curso='C',
             carga_horaria_disponibilidade=10,
             dias_disponibilidade='segunda',
             turnos_disponibilidade='manha',
-            email='m2@test',
+            email='mu@test',
             telefone_whatsapp='2',
-            codigo_acesso='DEF456',
-            cliente_id=cliente1.id,
+            codigo_acesso='NOP456',
         )
-        db.session.add_all([monitor1, monitor2])
+        db.session.add_all([monitor_linked, monitor_unlinked])
         db.session.commit()
 
-        atribuicao1 = MonitorPolo(
-            monitor_id=monitor1.id,
-            polo_id=polo_a.id,
+        atribuicao = MonitorPolo(
+            monitor_id=monitor_linked.id,
+            polo_id=polo.id,
             ativo=True,
         )
-        atribuicao2 = MonitorPolo(
-            monitor_id=monitor1.id,
-            polo_id=polo_c.id,
-            ativo=True,
-        )
-        db.session.add_all([atribuicao1, atribuicao2])
+        db.session.add(atribuicao)
         db.session.commit()
     yield app
 
@@ -120,9 +95,10 @@ def login_monitor_session(client, monitor_id):
         sess['_id'] = 'test-session'
         sess['user_type'] = 'monitor'
 
-def test_monitor_with_assignments(client, app):
+
+def test_monitor_infer_cliente_id(client, app):
     with app.app_context():
-        monitor = Monitor.query.filter_by(email='m1@test').first()
+        monitor = Monitor.query.filter_by(email='ml@test').first()
 
     with client:
         login_monitor_session(client, monitor.id)
@@ -132,17 +108,16 @@ def test_monitor_with_assignments(client, app):
         assert data['success'] is True
         assert len(data['materiais']) == 1
         assert data['materiais'][0]['nome'] == 'Material A'
-        assert 'Material C' not in [m['nome'] for m in data['materiais']]
 
 
-def test_monitor_without_assignments(client, app):
+def test_monitor_missing_association_error(client, app):
     with app.app_context():
-        monitor = Monitor.query.filter_by(email='m2@test').first()
+        monitor = Monitor.query.filter_by(email='mu@test').first()
 
     with client:
         login_monitor_session(client, monitor.id)
         resp = client.get('/api/materiais')
         data = resp.get_json()
-        assert resp.status_code == 200
-        assert data['success'] is True
-        assert data['materiais'] == []
+        assert resp.status_code == 400
+        assert data['success'] is False
+        assert 'não associado' in data['message']
