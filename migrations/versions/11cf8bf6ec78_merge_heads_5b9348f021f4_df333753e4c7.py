f5876e3f7cd279c67512a30b9fa7b5fd60673b89
cabralipe <cabralipe@outlook.com>
2025-09-12 16:40:37 -0300

    ajustes nos migrates

---
+++ migrations/versions/11cf8bf6ec78_merge_heads_5b9348f021f4_df333753e4c7.py
@@ -0,0 +1,24 @@
+"""Merge heads 5b9348f021f4 + df333753e4c7
+
+Revision ID: 11cf8bf6ec78
+Revises: 5b9348f021f4, df333753e4c7
+Create Date: 2025-09-12 16:39:49.733166
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '11cf8bf6ec78'
+down_revision = ('5b9348f021f4', 'df333753e4c7')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
