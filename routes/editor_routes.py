8c12251af5c6a48662984bf073d53676cc67b6c9
felippe11 <andrefelippe11@hotmail.com>
2025-09-19 11:42:50 -0300

    editor_routes

---
+++ routes/editor_routes.py
@@ -0,0 +1,7 @@
+from flask import Blueprint
+
+# Blueprint para rotas do editor
+editor_bp = Blueprint('editor', __name__, url_prefix='/editor')
+
+# Placeholder para futuras rotas do editor
+# As rotas específicas do editor podem ser adicionadas aqui conforme necessário
