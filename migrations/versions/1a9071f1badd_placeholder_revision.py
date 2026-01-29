db1cb8b570a0c952268353651904f1257557bf33
Felipe Cabral <cabralipe@outlook.com>
2026-01-29 16:35:35 -0300

    This version of Antigravity is no longer supported. Please update to receive the latest features!

---
+++ migrations/versions/1a9071f1badd_placeholder_revision.py
@@ -0,0 +1,22 @@
+"""placeholder for missing revision 1a9071f1badd
+
+Revision ID: 1a9071f1badd
+Revises: 9bac7f9f0073
+Create Date: 2026-01-29 00:00:00.000000
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+revision = "1a9071f1badd"
+down_revision = "9bac7f9f0073"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
